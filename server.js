9a095c155512822b6233fb88941752717363dc3e
cfpaige <cfpaige@gmail.com>
2019-07-16 15:53:21 -0700

    google login set up, not sequelized

--- server.js
+++ server.js
@@ -1,29 +1,42 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+const passport = require('./passportAuthentication');
+const apiRoutes = require('./routes/apiRoutes');
+const authenticationRoute = require('./routes/authentication');
+const htmlRoutes = require('./routes/htmlRoutes');
+const cookieSession = require('cookie-session');
 
 var db = require("./models");
 
-var app = express();
 var PORT = process.env.PORT || 3000;
+
+var app = express();
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000, // one day in miliseconds
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routes/apiRoutes")(app);
+// require("./routes/authentication")(app);
+// require("./routes/htmlRoutes")(app);
+
+app.use('/', authenticationRoute);
+app.use('/', apiRoutes);
+app.use('/', htmlRoutes);
 
 var syncOptions = { force: false };
 
