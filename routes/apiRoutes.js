e6b9bc0b28a067bc9eb328f6d0fc4aaeb1e6901f
strebz <helankjose@gmail.com>
2019-07-19 15:03:33 -0700

    saved api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,6 @@ const connection = require('../config/connection');
 require('dotenv').config();
 const axios = require('axios');
 const express = require('express');
-<<<<<<< HEAD
 var request = require('request');   
 //var app = express();
 const router = express.Router();
@@ -42,7 +41,6 @@ var db = require("../models");
   router.delete("/api/examples/:id", function(req, res) {
     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
       res.json(dbExample);
-=======
 const router = express.Router();
 
 // var users = require("../models/user.js");
@@ -129,10 +127,8 @@ const router = express.Router();
         }).catch(function (error) {
             console.log(error);
         })
->>>>>>> 1f23f6f1ddda2ce6a2a53362cb98af7f93f7b88a
     });
 
-<<<<<<< HEAD
   router.post("/api/college", function(req, res) {
      var city=req.body.city;
     var dept=req.body.dept;
@@ -146,7 +142,6 @@ const router = express.Router();
       return res.json(response.data.results);
     }).catch(function(error) {
       console.log(error);
-=======
     router.get("/api/apprenticeship/:place", function (req, res) {
         let place = req.params.place; //ex: Seattle,WA (city and state or just state)
         axios.get("https://api.careeronestop.org/v1/apprenticeshipfinder/" + process.env.COSID + "/" + place + "/25", { headers: { Authorization: "Bearer " + process.env.COSTOKEN } })
@@ -156,7 +151,6 @@ const router = express.Router();
                 console.log(err);
                 res.end()
             })
->>>>>>> 1f23f6f1ddda2ce6a2a53362cb98af7f93f7b88a
     })
 
     router.get("/api/certification/:field", function (req, res) {
@@ -171,7 +165,6 @@ const router = express.Router();
             })
     })
 
-<<<<<<< HEAD
 
 router.get("/api/college", function(req, res) {
   
@@ -221,7 +214,6 @@ function checkAuthentication(req, res, next) {
     if (isAuthenticate) {
         return next();
     }
-=======
     router.get("/api/licenses/:field/:location", function (req, res) {
         let field = req.params.field; //ex: doctors
         let location = req.params.location; //ex: WA (NOT city and state)
@@ -233,7 +225,6 @@ function checkAuthentication(req, res, next) {
                 console.log(error)
             })
     })
->>>>>>> 1f23f6f1ddda2ce6a2a53362cb98af7f93f7b88a
 
     // router.get("/api/apprenticeship/sponsers/:field/:place", function (req, res) {
     //   let field = req.params.field;
