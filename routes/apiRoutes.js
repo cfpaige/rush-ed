785ce8967b57934bb3bf81533492bf0d3c25bcef
dnl092 <drogalsky14@my.whitworth.edu>
2019-07-19 15:08:13 -0700

    need to pull

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,67 +5,6 @@ const axios = require('axios');
 const express = require('express');
 const router = express.Router();
 
-// var users = require("../models/user.js");
-// var favs = require("../models/fav.js");
-// var user_favs = require(../models/user_favs.js);
-
-
-// ================================= BASIC CRUD =================================
-
-// // Create all our routes and set up logic within those routes where required.
-// router.get("/", function(req, res) {
-//   cat.all(function(data) {
-//     var hbsObject = {
-//       cats: data
-//     };
-//     console.log(hbsObject);
-//     res.render("index", hbsObject);
-//   });
-// });
-
-// router.post("/api/cats", function(req, res) {
-//   cat.create([
-//     "name", "sleepy"
-//   ], [
-//     req.body.name, req.body.sleepy
-//   ], function(result) {
-//     // Send back the ID of the new quote
-//     res.json({ id: result.insertId });
-//   });
-// });
-
-// router.put("/api/cats/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
-
-//   console.log("condition", condition);
-
-//   cat.update({
-//     sleepy: req.body.sleepy
-//   }, condition, function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
-
-// router.delete("/api/cats/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
-
-//   cat.delete(condition, function(result) {
-//     if (result.affectedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
-// ==============================================================================
-
-
 
     // router.post("/places", function (req, res) {
     //     let placeQuery = req.body;
@@ -77,13 +16,13 @@ const router = express.Router();
     //     }).catch(function (error) {
     //         console.log(error);
     //     })
-    // });
+    // }); im not sure why this is still here? why are there two of them?
 
     router.post("/api/places", function (req, res) {
         let placeQuery = req.body;
         let latitude = placeQuery['location[latitude]'];
         let longitude = placeQuery['location[longitude]'];
-        axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=" + process.env.GPLACES + "&location=" + latitude + ',' + longitude + "&radius=1500&type=university"
+        axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=" + process.env.GOOGLEKEY + "&location=" + latitude + ',' + longitude + "&radius=1500&type=university"
         ).then(function (response) {
             res.json(response.data);
         }).catch(function (error) {
@@ -124,6 +63,10 @@ const router = express.Router();
             .catch(function (error) {
                 console.log(error)
             })
+    })
+
+    router.get("/api/calenderlist", function (req, res) {
+        
     })
 
     // router.get("/api/apprenticeship/sponsers/:field/:place", function (req, res) {
