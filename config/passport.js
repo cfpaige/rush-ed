d5b21aef24f04271c2a40dff7ecdbcb5126719c2
cfpaige <cfpaige@gmail.com>
2019-07-21 21:13:37 -0700

    fixed passport connection

--- config/passport.js
+++ config/passport.js
@@ -1,14 +1,11 @@
-var LocalStrategy = require("passport-local").Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 
-var db = require("../models");
+var db  = require('../models');
 
 module.exports = function (passport) {
 
-  // In order to help keep authentication state across HTTP requests,
-  // Sequelize needs to serialize and deserialize the user
-  // Just consider this part boilerplate needed to make it all work
-  passport.serializeUser(function (user, cb) {
-    cb(null, user);
+  passport.serializeUser(function (user, done) {
+    done(null, user);
   });
 
   passport.deserializeUser(function (email, done) {
@@ -16,22 +13,17 @@ module.exports = function (passport) {
       if (user) {
         done(null, user.get());
       } else {
-        // console.log("user.errors", user.errors)
         done(user.errors, null);
       }
     });
   });
 
-  // Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
-  passport.use('local-signup', new LocalStrategy(
-    // Our user will sign in using an email, rather than a "username"
-    {
-      usernameField: 'email',
-      passwordField: 'password',
-      passReqToCallback: true
-    },
-    function (req, email, password, done) {
-      // When a user tries to sign in this code runs
+  passport.use('local-signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'user_key',
+    passReqToCallback: true
+  },
+    function (req, email, user_key, done) {
       process.nextTick(function () {
         db.User.findOne({
           where: {
@@ -42,57 +34,42 @@ module.exports = function (passport) {
             console.log("err", err)
             return done(err);
           }
-
-          // check to see if theres already a user with that email
           if (user) {
-
             console.log('signupMessage', 'That email is already taken.');
             return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
           } else {
             db.User.create({
               email: req.body.email,
-              password: db.User.generateHash(password)
-
+              user_key: db.User.generateHash(user_key)
             }).then(function (dbUser) {
-              //console.log("created result: ", dbUser);
-              // send post back to render
               return done(null, dbUser);
-
             }).catch(function (err) {
-              // handle error;
               console.log(err);
             });
           }
         });
       });
-
     }));
 
-  passport.use('local-login', new LocalStrategy({
-    // by default, local strategy uses username and account_key, we will override with email
-    usernameField: 'email',
-    passwordField: 'password',
-    passReqToCallback: true
-  },
-    function (req, email, password, done) {
-
-      db.User.findOne({
-        where: {
-          email: req.body.email
-        }
-      }).then(function (user, err) {
-        if (!user) {
-          console.log("No user found.");
-          return done(null, false, req.flash('loginMessage', 'No user found.'));
-        }
-        if (user && !user.validPassword(req.body.password)) {
-
-          return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
-        }
-        return done(null, user);
-
-      });
-
+passport.use('local-login', new LocalStrategy({
+        usernameField: 'email',
+        passwordField : 'user_key',
+        passReqToCallback : true
+    },
+    function(req, email, user_key, done) {
+        db.User.findOne({
+            where: {
+                email: req.body.email 
+            }
+        }).then(function(user, err) {
+            if (!user){
+                console.log("No user found.");
+                return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+            }
+            if (user && !user.validPassword(req.body.user_key)){
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+            }
+            return done(null, user);
+        });
     }));
-
 };
--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var express = require('express');
 var session = require('express-session');
 var exphbs = require('express-handlebars');
 // Requiring passport as we've configured it
-var passport = require('./config/passport');
+var passport = require('passport');
+require('./config/passport');
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 3000;
